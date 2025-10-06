bad83470b8bf658cf31cb0347f61969377ab0058
lscomoon <lsco.moon25@gmail.com>
2025-10-06 16:38:49 +0330

    011
    011

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('testButton');
+  btn.addEventListener('click', () => {
+    alert('CSS و JS از گیت لود شدن ✅');
+  });
+});
